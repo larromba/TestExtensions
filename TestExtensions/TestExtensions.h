abdda9dc9ebab6dcab27fcd9b1510d794b3163cf
Lee Arromba <larromba@gmail.com>
2018-11-25 16:20:33 +0100

    initial commit

---
+++ TestExtensions/TestExtensions.h
@@ -0,0 +1,11 @@
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TestExtensions.
+FOUNDATION_EXPORT double TestExtensionsVersionNumber;
+
+//! Project version string for TestExtensions.
+FOUNDATION_EXPORT const unsigned char TestExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestExtensions/PublicHeader.h>
+
+
