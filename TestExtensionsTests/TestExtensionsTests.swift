8de35ede04cae3f564184c3e42ad1fc37f276da0
Lee Arromba <larromba@gmail.com>
2019-01-08 22:19:00 +0100

    + manual wait

--- TestExtensionsTests/TestExtensionsTests.swift
+++ TestExtensionsTests/TestExtensionsTests.swift
@@ -2,12 +2,22 @@
 import XCTest
 
 final class TestExtensionsTests: XCTestCase {
-    func testWait() {
+    func testWaitAutomaticCompletion() {
         let expectation = self.expectation(description: "wait callback invoked")
         let date = Date()
         wait(for: 1.0) {
             XCTAssertGreaterThanOrEqual(Date().timeIntervalSince(date), 1.0)
             expectation.fulfill()
+        }
+    }
+
+    func testWaitManualCompletion() {
+        let expectation = self.expectation(description: "wait callback invoked")
+        let date = Date()
+        wait(for: 1.0) { completion in
+            XCTAssertGreaterThanOrEqual(Date().timeIntervalSince(date), 1.0)
+            expectation.fulfill()
+            completion()
         }
     }
 }
