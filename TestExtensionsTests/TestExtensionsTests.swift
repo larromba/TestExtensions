73773543e952eae32c5938be25614dc97dbdc79d
Lee Arromba <larromba@gmail.com>
2019-01-08 01:09:51 +0100

    improve test

--- TestExtensionsTests/TestExtensionsTests.swift
+++ TestExtensionsTests/TestExtensionsTests.swift
@@ -4,7 +4,9 @@ import XCTest
 final class TestExtensionsTests: XCTestCase {
     func testWait() {
         let expectation = self.expectation(description: "wait callback invoked")
+        let date = Date()
         wait(for: 1.0) {
+            XCTAssertGreaterThanOrEqual(Date().timeIntervalSince(date), 1.0)
             expectation.fulfill()
         }
     }
